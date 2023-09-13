7ea89bdd19f9c6d4a8bdeb855edc5665dffa5f66
inmonim <cat1083014@gmail.com>
2023-09-13 04:12:31 +0000

    feat : User Create 기초 완성

--- backend/api/user.py
+++ backend/api/user.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
 
+import bcrypt
 
 from db_conn import session_open
 
@@ -7,6 +9,31 @@ from models.model import User
 
 
 router = APIRouter()
+
+@router.post("/create_user")
+async def create_user(request: Request):
+    user_data = await request.json()
+    
+    user_name = user_data['user_name']
+    user_id = user_data['user_id']
+    plain_pw = user_data['password']
+    role = user_data.get('role')
+    
+    salt = bcrypt.gensalt()
+    hashed_pw = bcrypt.hashpw(plain_pw.encode('utf-8'), salt)
+    
+    with session_open() as db:
+        user = User()
+        user.student_num = user_id
+        user.name = user_name
+        user.password = hashed_pw
+        user.role = role
+        
+        db.add(user)
+        db.commit()
+    
+    return JSONResponse(content={''})
+
 
 # DB 연결 테스트용
 @router.get("/{user_id:int}")
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import user
 
 app = FastAPI()
+
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 app.include_router(user.router, prefix='/users', tags=['users'])
