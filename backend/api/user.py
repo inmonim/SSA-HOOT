963826a9f1be5bd4b1a15749e25f6230298ba6eb
inmonim <cat1083014@gmail.com>
2023-09-15 07:11:52 +0000

    feat : User DTO 구현

--- backend/api/user.py
+++ backend/api/user.py
@@ -1,44 +1,41 @@
-from fastapi import APIRouter, Request
+# FastAPI
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel, constr
 
+# 모듈 및 패키지
 import bcrypt
 import re
 
+# DB
 from db_conn import session_open
 
+# model, DTO
 from models.model import User
+from dto.user import ( CreateUserDTO, LoginDTO, ModifyeUserDTO )
 
-class CreateUser(BaseModel):
-    user_name : str
-    user_id : str
-    password: str
-    role : str
-
+# router 등록
 router = APIRouter()
 
 # 계정 생성 요청
 @router.post("/create_user")
-async def create_user(request: Request, request_data: CreateUser):
-    user_data = await request_data
+async def create_user(create_user_data: CreateUserDTO):
     
-    user_name = user_data['user_name']
-    user_id = user_data['user_id']
-    plain_pw = user_data['password']
-    
-    role = user_data.get('role')
+    user_name = create_user_data.user_name
+    user_id = create_user_data.user_id
+    plain_pw = create_user_data.password
+    role = create_user_data.role
     
     # user_id 재검사
     check_id_response = check_valid_id(user_id)
     if check_id_response['status_code'] != 200:
-        return JSONResponse(content = check_id_response['content'],
-                            status_code = check_id_response['status_code'])
+        raise HTTPException(detail=check_id_response['detail'],
+                             status_code=check_id_response['status_code'])
         
-    # 비밀번호 유효성 검사
+    # 비밀번호 유효성 재검사
     check_pw_response = check_valid_password(plain_pw)
     if check_pw_response['status_code'] != 200:
-        return JSONResponse(content = check_pw_response['content'],
-                            status_code = check_pw_response['status_code'])
+        raise HTTPException(detail=check_pw_response['detail'],
+                             status_code=check_pw_response['status_code'])
     
     # 평문 비밀번호 암호화
     salt = bcrypt.gensalt()
@@ -51,10 +48,11 @@ async def create_user(request: Request, request_data: CreateUser):
         user.name = user_name
         user.password = hashed_pw
         user.role = role
+             
         db.add(user)
         db.commit()
     
-    return JSONResponse(content = {'message':'계정이 생성되었습니다.'},
+    return JSONResponse(content = {'detail':'계정이 생성되었습니다.'},
                         status_code = 200)
 
 # 아이디 유효성 검사 요청
@@ -65,66 +63,75 @@ async def check_valid_id_request(request:Request):
     
     user_id = data['user_id']
     
-    id_check_response = check_valid_id(user_id)
+    check_id_response = check_valid_id(user_id)
+    if check_id_response['status_code'] != 200:
+        raise HTTPException(detail=check_id_response['detail'],
+                             status_code=check_id_response['status_code'])
     
-    return JSONResponse(content=id_check_response['content'],
-                        status_code=id_check_response['status_code'])
+    return JSONResponse(content={'detail':check_id_response['detail']},
+                        status_code=check_id_response['status_code'])
 
 # 로그인 요청
 @router.post('/login')
-async def login(reqeust:Request):
+async def login(request_data: LoginDTO):
     
-    data = await reqeust.json()
-    
-    input_id = data['user_id']
-    input_pw = data['password']
+    input_id = request_data.user_id
+    input_pw = request_data.password
     
     with session_open() as db:
         
         user = db.query(User).filter(User.user_id == input_id).first()
         
+        # user_id가 DB에 없을 경우
         if not user:
-            return JSONResponse(content={'message':'계정이 없습니다.'},
+            raise HTTPException(detail = '존재하지 않는 계정입니다.',
                                 status_code=401)
         
+        # user_id로 DB에서 가져온 비밀번호 정보
         hashed_pw = user.password
         
+        # 비밀번호가 없을 경우
         if bcrypt.checkpw(input_pw.encode('utf-8'), hashed_pw.encode('utf-8')) == False:
-            return JSONResponse(content={'message':'비밀번호가 틀렸습니다.'},
+            raise HTTPException(detail = '비밀번호가 틀렸습니다.',
                                 status_code=401)
     
-    return JSONResponse(content={'message':'로그인 성공'},
+    return JSONResponse(content={'detail':'로그인 성공'},
                         status_code=200)
 
 # 계정 정보 수정
 @router.put('/modify_user')
-async def modify_user(request:Request):
+def modify_user(request_data:ModifyeUserDTO):
     
-    data = await request.json()
+    user_id = request_data.user_id
+    new_name = request_data.user_name
+    new_plain_pw = request_data.password
+    new_role = request_data.role
     
     with session_open() as db:
-        user = db.query(User).filter(User.user_id == data['user_id']).first()
+        user = db.query(User).filter(User.user_id == user_id).first()
         
-        if new_name := data.get('name'):
+        if new_name:
             user.name = new_name
         
-        if new_role := data.get('role'):
+        if new_role:
             user.role = new_role
         
-        if plain_pw := data.get('password'):
+        if new_plain_pw:
             
             # 비밀번호 유효성 검사
-            check_pw_response = check_valid_password(plain_pw)
+            check_pw_response = check_valid_password(new_plain_pw)
             if check_pw_response['status_code'] != 200:
-                return JSONResponse(content = check_pw_response['content'],
+                raise JSONResponse(content = check_pw_response['detail'],
                                     status_code = check_pw_response['status_code'])
+                
+            # 유효성 검사 통과 시 비밀번호 재생성
             salt = bcrypt.gensalt()
-            hashed_pw = bcrypt.hashpw(plain_pw.encode('utf-8'), salt)
+            hashed_pw = bcrypt.hashpw(new_plain_pw.encode('utf-8'), salt)
             user.password = hashed_pw
 
         db.commit()
     
-    return JSONResponse(content={'message' : '개인정보 수정이 완료되었습니다.'},
+    return JSONResponse(content={'detail' : '개인정보 수정이 완료되었습니다.'},
                         status_code=200) 
 
 
@@ -135,15 +142,15 @@ def check_valid_id(user_id:int):
         
         # DB 내 중복 user_id 검사
         if db.query(User).filter(User.user_id == user_id).count():
-            return {'content':{'message' : '중복된 ID입니다.'}, 'status_code':409}
+            return {'detail': '중복된 ID입니다.', 'status_code':409}
     
     # user_id 유효성 검사
     # user_id는 영문과 숫자를 혼합하여 6자 이상
     pattern = r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$'
     if not bool(re.match(pattern, user_id)):
-        return {'content':{'message' : 'ID는 영문자와 숫자를 포함하여 6자 이상입니다.'}, 'status_code' : 409}
+        return {'detail' : 'ID는 영문자와 숫자를 포함하여 6자 이상입니다.', 'status_code' : 409}
     
-    return {'content' : {'message' : '사용할 수 있는 ID입니다.'}, 'status_code': 200}
+    return {'detail': '사용할 수 있는 ID입니다.', 'status_code': 200}
 
 
 # 비밀번호 유효성 검사 함수
@@ -152,16 +159,6 @@ def check_valid_password(password:str):
     # 비밀번호는 영문과 숫자를 포함하여 8자 이상
     pattern = r'^(?=.*[a-zA-Z])(?=.*\d).{8,}$'
     if not bool(re.match(pattern, password)):
-        return {'content':{'message' : '비밀번호는 영문자와 숫자를 포함하여 8자 이상입니다.'}, 'status_code' : 409}
+        return {'detail' : '비밀번호는 영문자와 숫자를 포함하여 8자 이상입니다.', 'status_code' : 409}
     
-    return {'content' : {'message' : '사용할 수 있는 비밀번호입니다.'}, 'status_code': 200}
-
-
-
-# DB 연결 테스트용
-@router.get("/{user_id:int}")
-def get_user(user_id:int):
-    with session_open() as db:
-        user = db.query(User).get(user_id)
-    
-    return {'result':user}
+    return {'detail' : '사용할 수 있는 비밀번호입니다.', 'status_code': 200}
--- backend/dto/user.py
+++ backend/dto/user.py
@@ -1,7 +1,20 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, Field
 
-class CreateUser(BaseModel):
+
+class CreateUserDTO(BaseModel):
     user_name : str
-    user_id : str
-    password: str
-    role : str
+    user_id : str = Field(..., min_length=6, max_length=20, pattern=r"^[a-zA-Z0-9]{6,}$", description="id는 영문과 숫자를 포함해 6자 이상 20자 이하") 
+    password: str = Field(..., min_length=8, max_length=20, pattern=r"^[a-zA-Z0-9\d!@#$%^&*]{8,}$", description="비밀번호는 영문과 숫자를 포함해 8자 이상 20자 이하")
+    role : str = "교육생"
+    
+
+class LoginDTO(BaseModel):
+    user_id : str = Field(..., min_length=6, max_length=20, pattern=r"^[a-zA-Z0-9]{6,}$", description="id는 영문과 숫자를 포함해 6자 이상 20자 이하") 
+    password : str = Field(..., min_length=8, max_length=20, pattern=r"^[a-zA-Z0-9\d!@#$%^&*]{8,}$", description="비밀번호는 영문과 숫자를 포함해 8자 이상 20자 이하")
+
+
+class ModifyeUserDTO(BaseModel):
+    user_name : str = None
+    user_id : str = Field(..., min_length=6, max_length=20, pattern=r"^[a-zA-Z0-9]{6,}$", description="id는 영문과 숫자를 포함해 6자 이상 20자 이하") 
+    password : str = Field(None, min_length=8, max_length=20, pattern=r"^[a-zA-Z0-9\d!@#$%^&*]{8,}$", description="비밀번호는 영문과 숫자를 포함해 8자 이상 20자 이하")
+    role : str = None
